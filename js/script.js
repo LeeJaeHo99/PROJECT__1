6bdd8b9dbe91db331b48049fd64431e7a63ab52c
Leejaeho1234 <ljh2735294@naver.com>
2024-02-09 05:54:11 +0900

    반응형

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const btn = document.querySelector('.popup>span');
 btn.onclick = function(){
     pop.style.display = 'none';
 };
+
+$('.o').on('click', function () {
+    $('html').addClass('all_scrollFixed');
+    $('#sitemap').show();
+    $('.x').on('click', function () {
+    $('#sitemap').hide();
+    $('html').removeClass('all_scrollFixed');
+    });
+})
